92fc8a5dbf4a47e8b0d2af61d6fc3adf02c20e26
akshayavelma <akshayareddy249@email.com>
2024-11-20 19:16:21 +0530

    first commit

---
+++ CheckPrime.java
@@ -0,0 +1,24 @@
+public class CheckPrime {
+    public static void main(String[] args) {
+        int number = 5; // The number to check
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
